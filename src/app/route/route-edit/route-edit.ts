f3413a4fa47ff6309cba1291d2a6f705b6ecdbaf
Pawel Wujczyk <pwujczyk@gmail.com>
2026-05-20 22:18:13 +0200

    route edit

--- src/app/route/route-edit/route-edit.ts
+++ src/app/route/route-edit/route-edit.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Route } from '../models/route';
 
 @Component({
   selector: 'app-route-edit',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './route-edit.html',
   styleUrl: './route-edit.css',
 })
-export class RouteEdit {}
+export class RouteEdit {
+  route: Route = new Route(-1, '');
+}
