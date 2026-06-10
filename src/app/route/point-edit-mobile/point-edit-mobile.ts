bc635c51116429baa9a4daae23bf33b246327e2e
Pawel Wujczyk <pwujczyk@gmail.com>
2026-06-10 21:02:58 +0000

    waypoints mobile changes

--- src/app/route/point-edit-mobile/point-edit-mobile.ts
+++ src/app/route/point-edit-mobile/point-edit-mobile.ts
@@ -1,9 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Point } from '../models/point';
+
 
 @Component({
   selector: 'app-point-edit-mobile',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './point-edit-mobile.html',
   styleUrl: './point-edit-mobile.css',
 })
-export class PointEditMobile {}
+export class PointEditMobile {
+  @Input() point!: Point;
+  @Input() points: Point[] = [];
+  @Input() index!: number;
+
+  onOdometerChange(newOdometer: number) {
+    console.log('Odometer changed: ', newOdometer);
+    this.point.odometer = newOdometer;
+
+    if (this.index > 0 && this.points && this.points[this.index - 1]) {
+      const prevOdometer = this.points[this.index - 1].odometer || 0;
+      this.point.distance = newOdometer - prevOdometer;
+    }
+  }
+}
